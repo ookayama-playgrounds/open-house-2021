e85d1d8a6ea6ea2b3ec401d2a00785c8d695625a
Riku Takano <rick.takano@gmail.com>
2021-03-04 18:05:41 +0900

    add primevue and primeicon

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+// ui framework
+import PrimeVue from 'primevue/config';
+
 createApp(App)
   .use(router)
+  .use(PrimeVue, {ripple: true})
   .mount("#app");
